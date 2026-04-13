a1432e5b0e1cbb77a5d6ec5846f53c5b9629a03c
Mahad Hassan <hamaxa@syntronic.com>
2026-04-13 15:06:16 +0200

    Modified sensor node header

---
+++ include/sensor_node.h
@@ -0,0 +1,26 @@
+#ifndef SENSOR_NODE_H
+#define SENSOR_NODE_H
+
+#include "stdint.h"
+#include "readings.h"
+
+typedef enum
+{
+    BOOT,
+    NORMAL,
+    TRANSMIT,
+    ALERT,
+    ERROR
+} sensor_node_state_t;
+
+typedef struct 
+{
+    sensor_node_state_t curr_state;
+    bool anomaly_detected;
+    bool boot_complete;
+} sensor_node_t;
+
+void init(sensor_node_t* sn);
+void update(sensor_node_t* sn);
+
+#endif /* SENSOR_NODE_H */
